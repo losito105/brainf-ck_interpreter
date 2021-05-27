bc0bd089acd7b9dc7ea230fc1de039b0e1f95bcd
Tim Losito <43625213+losito105@users.noreply.github.com>
2021-05-27 06:07:45 -0400

    added hello world output file

---
+++ hello_world.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+int main(int argc, char *argv[]){
+char array[30000] = {0};
+char *ptr = array;
+(*(char *)ptr)++;
+while(*(char *)ptr){
+(*(char *)ptr)--;
+(*(char *)ptr)--;
+ptr++;
+(*(char *)ptr)--;
+while(*(char *)ptr){
+ptr++;
+ptr++;
+(*(char *)ptr)++;
+ptr++;
+(*(char *)ptr)--;
+(*(char *)ptr)--;
+(*(char *)ptr)--;
+(*(char *)ptr)--;
+(*(char *)ptr)--;
+ptr--;
+ptr--;
+}
+ptr--;
+(*(char *)ptr)--;
+(*(char *)ptr)--;
+ptr--;
+(*(char *)ptr)--;
+(*(char *)ptr)--;
+(*(char *)ptr)--;
+}
+ptr++;
+(*(char *)ptr)--;
+putchar(*(char *)ptr);
+ptr++;
+ptr++;
+ptr++;
+(*(char *)ptr)++;
+putchar(*(char *)ptr);
+ptr++;
+ptr++;
+putchar(*(char *)ptr);
+putchar(*(char *)ptr);
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+while(*(char *)ptr){
+putchar(*(char *)ptr);
+ptr++;
+}
+ptr--;
+ptr--;
+ptr--;
+ptr--;
+putchar(*(char *)ptr);
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+putchar(*(char *)ptr);
+(*(char *)ptr)--;
+(*(char *)ptr)--;
+(*(char *)ptr)--;
+(*(char *)ptr)--;
+(*(char *)ptr)--;
+(*(char *)ptr)--;
+putchar(*(char *)ptr);
+ptr--;
+ptr--;
+(*(char *)ptr)--;
+putchar(*(char *)ptr);
+ptr++;
+ptr++;
+ptr++;
+ptr++;
+(*(char *)ptr)++;
+putchar(*(char *)ptr);
+}

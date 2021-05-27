5ff4c128bb53eda788dc392b674052db2b9d7bf7
Tim Losito <43625213+losito105@users.noreply.github.com>
2021-05-27 06:11:07 -0400

    added cell size output file

---
+++ cell_size.c
@@ -0,0 +1,293 @@
+#include <stdio.h>
+int main(int argc, char *argv[]){
+char array[30000] = {0};
+char *ptr = array;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+while(*(char *)ptr){
+ptr++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+ptr--;
+(*(char *)ptr)--;
+}
+ptr++;
+while(*(char *)ptr){
+ptr--;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+ptr++;
+(*(char *)ptr)--;
+}
+(*(char *)ptr)++;
+ptr--;
+while(*(char *)ptr){
+ptr++;
+(*(char *)ptr)--;
+ptr--;
+while(*(char *)ptr){
+ptr++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+ptr--;
+(*(char *)ptr)--;
+}
+ptr++;
+while(*(char *)ptr){
+ptr--;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+ptr++;
+(*(char *)ptr)--;
+}
+ptr--;
+while(*(char *)ptr){
+ptr++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+ptr--;
+(*(char *)ptr)--;
+}
+(*(char *)ptr)++;
+ptr++;
+while(*(char *)ptr){
+ptr++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+while(*(char *)ptr){
+ptr++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+ptr--;
+(*(char *)ptr)--;
+}
+ptr++;
+(*(char *)ptr)++;
+putchar(*(char *)ptr);
+(*(char *)ptr)--;
+putchar(*(char *)ptr);
+while(*(char *)ptr){
+(*(char *)ptr)--;
+}
+ptr--;
+ptr--;
+while(*(char *)ptr){
+(*(char *)ptr)--;
+}
+ptr--;
+(*(char *)ptr)--;
+ptr++;
+}
+ptr--;
+while(*(char *)ptr){
+ptr++;
+ptr++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+while(*(char *)ptr){
+ptr++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+ptr--;
+(*(char *)ptr)--;
+}
+ptr++;
+putchar(*(char *)ptr);
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+putchar(*(char *)ptr);
+while(*(char *)ptr){
+(*(char *)ptr)--;
+}
+ptr--;
+ptr--;
+ptr--;
+(*(char *)ptr)--;
+}
+}
+ptr++;
+while(*(char *)ptr){
+ptr++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+while(*(char *)ptr){
+ptr++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+ptr--;
+(*(char *)ptr)--;
+}
+ptr++;
+putchar(*(char *)ptr);
+while(*(char *)ptr){
+(*(char *)ptr)--;
+}
+ptr--;
+ptr--;
+(*(char *)ptr)--;
+}
+ptr--;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+while(*(char *)ptr){
+ptr++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+ptr++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+ptr++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+ptr++;
+(*(char *)ptr)++;
+ptr--;
+ptr--;
+ptr--;
+ptr--;
+(*(char *)ptr)--;
+}
+ptr++;
+(*(char *)ptr)--;
+putchar(*(char *)ptr);
+ptr++;
+(*(char *)ptr)--;
+putchar(*(char *)ptr);
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+putchar(*(char *)ptr);
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+putchar(*(char *)ptr);
+ptr--;
+putchar(*(char *)ptr);
+ptr++;
+ptr++;
+putchar(*(char *)ptr);
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+putchar(*(char *)ptr);
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+(*(char *)ptr)++;
+putchar(*(char *)ptr);
+putchar(*(char *)ptr);
+ptr--;
+(*(char *)ptr)--;
+putchar(*(char *)ptr);
+ptr++;
+ptr++;
+(*(char *)ptr)--;
+putchar(*(char *)ptr);
+while(*(char *)ptr){
+while(*(char *)ptr){
+(*(char *)ptr)--;
+}
+ptr--;
+}
+}
